1210350b6c51ecbcbc215f053a6d84008121584b
gaobinchuan <gaobinchuan@sina.com>
2016-03-21 00:23:33 +0800

    fix mac os bugs

--- lib/mkvoice.sh
+++ lib/mkvoice.sh
@@ -6,7 +6,7 @@ for (( i = 1; i < $COUNT; i = i + 1 ))
 do
     en=`sed -n $(( i * 2 + 1 )),1p ~/.Gwords/list.txt | tr [:space:] ' '`
     cn=`sed -n $(( i * 2 + 2 )),1p ~/.Gwords/list.txt | tr [:space:] ' '`
-    echo $en : $cn
-    gr $en
+    echo '================================================'
+    gd $en
     sleep 7
 done
