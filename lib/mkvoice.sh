7cd960b8fd2db41dccddb7e6dc542846f382aa38
gaobinchuan <gaobinchuan@sina.com>
2016-03-24 03:04:56 +0800

    fix mkvoice again

--- lib/mkvoice.sh
+++ lib/mkvoice.sh
@@ -7,7 +7,8 @@ do
     en=`sed -n $(( i * 2 + 1 )),1p ~/.Gwords/list.txt | tr [:space:] ' '`
     cn=`sed -n $(( i * 2 + 2 )),1p ~/.Gwords/list.txt | tr [:space:] ' '`
     echo '================================================'
-    gw $en
     gd $en
-    sleep 7
+    sleep 6
+    gw $en
+    sleep 6
 done
